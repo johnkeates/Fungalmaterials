1885ddd06236463002d4390c5554b467ebca8575
Realitijd <derealitijd@gmail.com>
2024-09-06 12:10:48 +0200

    Added admin organisation to the project

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Species, Substrate, Topic, Method, Article
 
-# Register your models here.
+# Species
+@admin.register(Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    list_display = ('name',)  # Fields to display in the list view
+    search_fields = ('name', 'alternative_names')  # Searchable fields
+    ordering = ('name',)  # Order by the 'name' field
+
+
+# Substrate
+@admin.register(Substrate)
+class SubstrateAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+# Topic
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+# Growth method
+@admin.register(Method)
+class MethodAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+# Article
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'journal', 'approved')
+    list_filter = ('approved', 'year', 'species', 'substrate', 'topic', 'method')
+    search_fields = ('title', 'authors', 'doi', 'journal')
+    ordering = ('-year', 'title')  # Order by 'year' (descending) and 'title'
+    filter_horizontal = ('species', 'substrate', 'topic', 'method')  # Horizontal filter for many-to-many fields
+    autocomplete_fields = ('species', 'substrate')
