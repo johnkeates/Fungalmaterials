f24804645c2abf45805af6f33d7d44096c98bd6d
Realitijd <derealitijd@gmail.com>
2024-09-28 11:35:21 +0200

    Changed max_length of Abstract from 1500 to 2000

--- fungalmaterials/models.py
+++ fungalmaterials/models.py
@@ -86,7 +86,7 @@ class Article(Date):
 	journal = models.CharField(max_length=100, blank=True)
 	doi = models.URLField(max_length=100, unique=True, blank=True)
 	# pdf = models.FileField(blank=True, null=True)
-	abstract = models.TextField(max_length=1500, blank=True)
+	abstract = models.TextField(max_length=2000, blank=True)
 	species = models.ManyToManyField(Species, blank=True)
 	substrate = models.ManyToManyField(Substrate, blank=True, verbose_name="Substrate/Medium")
 	topic = models.ManyToManyField(Topic, blank=True)
